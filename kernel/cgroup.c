31a7775d5a1ad29c3646be0ef8c743c3b26b0ce6
Colin Cross <ccross@android.com>
2010-11-23 21:37:04 -0800

    cgroup: Remove call to synchronize_rcu in cgroup_attach_task
    synchronize_rcu can be very expensive, averaging 100 ms in
    some cases.  In cgroup_attach_task, it is used to prevent
    a task->cgroups pointer dereferenced in an RCU read side
    critical section from being invalidated, by delaying the
    call to put_css_set until after an RCU grace period.
    
    To avoid the call to synchronize_rcu, make the put_css_set
    call rcu-safe by moving the deletion of the css_set links
    into free_css_set_work, scheduled by the rcu callback
    free_css_set_rcu.
    
    The decrement of the cgroup refcount is no longer
    synchronous with the call to put_css_set, which can result
    in the cgroup refcount staying positive after the last call
    to cgroup_attach_task returns.  To allow the cgroup to be
    deleted with cgroup_rmdir synchronously after
    cgroup_attach_task, have rmdir check the refcount of all
    associated css_sets.  If cgroup_rmdir is called on a cgroup
    for which the css_sets all have refcount zero but the
    cgroup refcount is nonzero, reuse the rmdir waitqueue to
    block the rmdir until free_css_set_work is called.
    
    Signed-off-by: Colin Cross <ccross@android.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -3480,6 +3480,21 @@ static int cgroup_clear_css_refs(struct cgroup *cgrp)
 	return !failed;
 }
 
+/* checks if all of the css_sets attached to a cgroup have a refcount of 0.
+ * Must be called with css_set_lock held */
+static int cgroup_css_sets_empty(struct cgroup *cgrp)
+{
+	struct cg_cgroup_link *link;
+
+	list_for_each_entry(link, &cgrp->css_sets, cgrp_link_list) {
+		struct css_set *cg = link->cg;
+		if (atomic_read(&cg->refcount) > 0)
+			return 0;
+	}
+
+	return 1;
+}
+
 static int cgroup_rmdir(struct inode *unused_dir, struct dentry *dentry)
 {
 	struct cgroup *cgrp = dentry->d_fsdata;
